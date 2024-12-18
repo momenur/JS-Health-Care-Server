5316f283eb24d703ca0cdfb92d471fc84cf600af
momenur <momenurislam6@gmail.com>
2024-12-19 00:42:14 +0600

    uncoment

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -10,24 +10,24 @@ const router = express.Router();
 
 router.get(
   '/',
-  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   UserController.getAllUser,
 );
 
 router.get(
   '/me',
-  // auth(
-  //   ENUM_USER_ROLE.ADMIN,
-  //   ENUM_USER_ROLE.DOCTOR,
-  //   ENUM_USER_ROLE.PATIENT,
-  //   ENUM_USER_ROLE.SUPER_ADMIN,
-  // ),
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.DOCTOR,
+    ENUM_USER_ROLE.PATIENT,
+    ENUM_USER_ROLE.SUPER_ADMIN,
+  ),
   UserController.getMyProfile,
 );
 
 router.post(
   '/create-doctor',
-  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   FileUploadHelper.upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = UserValidation.createDoctor.parse(JSON.parse(req.body.data));
@@ -37,7 +37,7 @@ router.post(
 
 router.post(
   '/create-admin',
-  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   FileUploadHelper.upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = UserValidation.createAdmin.parse(JSON.parse(req.body.data));
