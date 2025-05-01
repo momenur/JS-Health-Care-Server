544a532b67537fdb5f004838ccee95de7133760f
momenur <momenurislam6@gmail.com>
2025-05-02 01:09:38 +0600

    Fixed.

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/v1', routes);
+app.use('/api/v1', routes);
 
 app.get('/', async (req: Request, res: Response) => {
   res.status(200).json({
